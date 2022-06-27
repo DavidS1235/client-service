139e59328ad6124ce74d85f69a87a83e39d988f3
davidsc <davidsilvacordova@gmail.com>
2022-06-27 12:59:16 -0500

    Added kafka config

---
+++ src/main/java/com/ds/clientservice/config/KafkaStringConfig.java
@@ -0,0 +1,28 @@
+package com.ds.clientservice.config;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+public class KafkaStringConfig {
+
+  public ProducerFactory<String,String> producerFactory(){
+
+    Map<String,Object> config = new HashMap<>();
+    config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+    config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    return new DefaultKafkaProducerFactory<>(config);
+  }
+
+  @Bean(name = "kafkaStringTemplate")
+  public KafkaTemplate<String,String> kafkaTemplate(){
+    return new KafkaTemplate<>(producerFactory());
+  }
+
+}
